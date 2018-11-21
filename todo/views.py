5b6a9bd71cf518509d268a0d9013468ad89a7006
Ray Zhu <jz6868@me.com>
2018-11-21 13:47:08 +0000

    two errors have been found and corrected: ?P; edit_an_item(request, id)

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse, redirect
+from django.shortcuts import render, HttpResponse, redirect, get_object_or_404
 from .models import Item
 from .forms import ItemForm
 
@@ -18,6 +18,10 @@ def create_an_item(request):
             return redirect(get_todo_list)
     else:
         form =ItemForm()
-        
-        
     return render(request, "item_form.html", {'form': form})
+    
+def edit_an_item(request, id):
+    item = get_object_or_404(Item, pk=id)
+    form = ItemForm(instance=item)
+    return render(request, "item_form.html", {'form': form})
+    
